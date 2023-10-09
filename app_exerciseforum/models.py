97c25966218b0e0f362356104fa6ca6ba09cb0ec
Maru <marusheik@gmail.com>
2023-10-09 09:35:30 +0200

    new model Planguage

--- app_exerciseforum/models.py
+++ app_exerciseforum/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Comment(models.Model):
     fecha = models.DateField()
     content = models.TextField()
+    
+class ProgLanguage(models.Model):
+    name = models.CharField(max_length=30)
