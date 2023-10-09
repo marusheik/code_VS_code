27ff6be32e33bd396e2b8cc54cfbb1b3c469120c
Maru <marusheik@gmail.com>
2023-10-09 15:44:07 +0200

    log in works

--- app_autentication/urls.py
+++ app_autentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import VRegister, vLog_Out
+from .views import VRegister, VLog_Out, VLogin
 from app_code_vs_code.views import loadIndex
 
 
 urlpatterns = [
     path('register/',VRegister.as_view(), name="Register"),
-    path('logout',vLog_Out, name="logout"),
+    path('login/',VLogin, name="login"),
+    path('logout',VLog_Out, name="logout"),
     path('', loadIndex, name='index'),
 ]
--- app_autentication/views.py
+++ app_autentication/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 from django.urls import reverse 
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-def vLog_Out(request):
+def VLogin(request):
+    
+    if request.method=="POST":
+        form=AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request,username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.error(request,"usuario no valido")
+        else:
+            messages.error(request,"info incorrecta")
+            
+    form = AuthenticationForm()
+    return render( request, "login/login.html", {"form":form})
+   
+def VLog_Out(request):
         logout(request)
         return redirect('index')
+    
 class VRegister(View):
     
     def get(self, request):
